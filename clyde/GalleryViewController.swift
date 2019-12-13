39dc010759e53000997943dd07c3bc6fdd870089
rahimimn <rahimimn@cofc.edu>
2019-12-13 14:50:09 -0500

    Added Gallery

--- clyde/GalleryViewController.swift
+++ clyde/GalleryViewController.swift
@@ -12,62 +12,48 @@ import SwiftyJSON
 
 class GalleryViewController: UIViewController {
 
-    @IBOutlet weak var image: UIImageView!
+    @IBOutlet var imageViewList: [UIImageView]!
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Adds the menu bar
         self.menuBar(menuBarItem: menuBarButton)
         // Adds the cofc logo to the nav
         self.addLogoToNav()        // Do any additional setup after loading the view.
-        dostuff()
-        //doDifferentStuff()
-        urlToImage(imageUrlString: "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000dtUX&oid=00D540000001Vbx&lastMod=1564422940000")
+        scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.view.frame.height+780)
+        
+        importGallery()
     }
     
-
+    var imageAddresses = ["https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nN25&oid=00D540000001Vbx&lastMod=1576246546000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nN2K&oid=00D540000001Vbx&lastMod=1576246692000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nN2e&oid=00D540000001Vbx&lastMod=1576247060000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nN2o&oid=00D540000001Vbx&lastMod=1576247171000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nN2t&oid=00D540000001Vbx&lastMod=1576247208000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nN33&oid=00D540000001Vbx&lastMod=1576247331000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nN41&oid=00D540000001Vbx&lastMod=1576247415000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nN5E&oid=00D540000001Vbx&lastMod=1576247828000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nN5x&oid=00D540000001Vbx&lastMod=1576248302000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nN62&oid=00D540000001Vbx&lastMod=1576248397000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nN6C&oid=00D540000001Vbx&lastMod=1576248512000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nN6M&oid=00D540000001Vbx&lastMod=1576248566000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nNLc&oid=00D540000001Vbx&lastMod=1576257552000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nNLm&oid=00D540000001Vbx&lastMod=1576257642000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nNLr&oid=00D540000001Vbx&lastMod=1576257704000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nNLw&oid=00D540000001Vbx&lastMod=1576257760000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nNfB&oid=00D540000001Vbx&lastMod=1576262465000","https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nNff&oid=00D540000001Vbx&lastMod=1576263081000", "https://c.cs40.content.force.com/servlet/servlet.ImageServer?id=01554000000nNgE&oid=00D540000001Vbx&lastMod=1576263582000"]
     
-    //https://cs40.salesforce.com/services/data/v46.0/sobjects/Document/01554000000dtUXAAY/body -H "Authorization: Bearer token"
     
-    func doDifferentStuff(){
-        let request = RestRequest(method: .GET, serviceHostType: .instance, path:"/services/data/v46.0/sobjects/Document/01554000000dtUXAAY", queryParams: nil)
-        RestClient.shared.send(request: request, onFailure: { (error, urlResponse) in
-            SalesforceLogger.d(type(of:self), message:"Error invoking: \(request)")
-            print(error)
-        }) {(self, response) in
-            os_log("\nSuccessful response received")
-            print(response)
+    
+    
+    
+    func importGallery(){
+        var count = 0
+        let index = imageAddresses.count
+        let numbers = randomSequenceGenerator(min: 0, max: index-1)
+        for image in imageViewList{
+            var num = numbers()
+            urlToImage(imageUrlString: imageAddresses[num], image: image)
         }
-    }
-
-    func dostuff(){
-        
-        
-       print("do stuff")
-        
-        //let data = Data(base64Encoded: imageData)!
-        //let imaged = UIImage(data: data)
-        
-        //image.image = imaged
-        
-        
-        //let pushRequest = RestClient.shared.request(forFileContents: "06854000000TCVr", version: nil)
-        //let pushRequest = RestClient.shared.request(forQuery: "SELECT ContentDownloadUrl,DistributionPublicUrl,PdfDownloadUrl FROM ContentDistribution")
-       // let pushRequest = RestClient.shared.request(forQuery: "SELECT VersionData FROM ContentVersion WHERE FirstPublishLocationId = '058540000006unNAAQ'")
-        let pushRequest = RestClient.shared.request(forQuery: "SELECT Body FROM Document WHERE Id = '01554000000dtUX'")
-        RestClient.shared.send(request: pushRequest, onFailure: {(error, urlResponse) in
-            print(error)
-
-        }) { [weak self](response, urlResponse) in
-            let jsonResponse = JSON(response!)
-            print(jsonResponse)
-
-
-            }
         
     }
     
-    func urlToImage(imageUrlString: String){
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func urlToImage(imageUrlString: String, image: UIImageView){
         let url = URL(string: imageUrlString)!
         
         let task = URLSession.shared.dataTask(with: url){ data,response, error in
@@ -76,12 +62,29 @@ class GalleryViewController: UIViewController {
                 return}
             print(error)
             DispatchQueue.main.async {
-                print(url)
-                self.image.image = UIImage(data:data)
+                image.image = UIImage(data:data)
             }
         }
         task.resume()    }
     
 
+    
+    /// Random sequnce of integers based on your range parameter. This was pulled and adapted from stack overflow: https://stackoverflow.com/questions/26457632/how-to-generate-random-numbers-without-repetition-in-swift
+    ///
+    /// - Parameters:
+    ///   - min: min number in range
+    ///   - max: max number in range
+    /// - Returns: returns a sequence of ints
+    func randomSequenceGenerator(min: Int, max: Int) -> () -> Int {
+        var numbers: [Int] = []
+        return {
+            if numbers.isEmpty {
+                numbers = Array(min ... max)
+            }
+            
+            let index = Int(arc4random_uniform(UInt32(numbers.count)))
+            return numbers.remove(at: index)
+        }
+    }
 }
 
