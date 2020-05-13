279ba2f448c5dfa8a099c77fe3a5d17887b735b2
rahimimn <rahimimn@cofc.edu>
2020-05-13 14:37:10 -0400

    QuestionViewController Documented

--- clyde/QuestionsViewController.swift
+++ clyde/QuestionsViewController.swift
@@ -12,17 +12,28 @@ import SalesforceSDKCore
 import SwiftyJSON
 
  //TO-DO: Create a chat system between user and counselors or tour guides.
-
+//A
+///Questions View Controller
+///
+/// Currently only presents a view with a button that shows the CofC Admissions FAQ
+///
+/// Idea #1: Create a chat system between user and counselors
+///
+/// Idea #2: Create a chat system between user and tour guides during a Campus Tour
+///
+/// Idea #3: Create a FAQ query system
 class QuestionsViewController: UIViewController, UITextFieldDelegate {
 
     //----------------------------------------------------------
     // MARK: Outlets
-    
+    //Menu button
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    //Button that links to the FAQ on CofC website
     @IBOutlet weak var linkToFaq: UIButton!
     
     //----------------------------------------------------------
     // MARK: Button Action
+    //Presents the CofC Admissions FAQ
     @IBAction func toFaq(_ sender: UIButton) {
        show("http://admissions.cofc.edu/faq/index.php")
     }
@@ -32,9 +43,12 @@ class QuestionsViewController: UIViewController, UITextFieldDelegate {
     // MARK: View functions
     
     override func viewDidLoad() {
-        super.viewDidLoad()        //reveals menu
+        super.viewDidLoad()
+        //reveals menu
         self.menuBar(menuBarItem: menuBarButton)
+        //adds logo to the nav
         self.addLogoToNav()
+        //Button configuration
         linkToFaq.titleLabel?.numberOfLines = 3
         linkToFaq.titleLabel?.lineBreakMode = .byWordWrapping
         linkToFaq.titleLabel?.adjustsFontSizeToFitWidth = true
